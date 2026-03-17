4075e130ad5130856c4f075f868bb9f16ddf106c
igorhajduk35 <igorhajduk35@gmail.com>
2026-03-17 14:57:28 +0100

    Added postgresql

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import psycopg2
 
+db_connection = psycopg2.connect(
+    host="db",
+    database="mydb",
+    user="myuser",
+    password="mypassword"
+)
 
 app = Flask(__name__)
 CORS(app)
@@ -8,8 +15,10 @@ CORS(app)
 
 @app.route("/api")
 def hello():
-    return jsonify({"message": "Hello there"})
-
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT 'Hello from PostgreSQL!' ")
+    result = cursor.fetchone()
+    return jsonify({"message": result[0]})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5050)
