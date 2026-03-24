acc6b651073c7cb16952360a6a861af68d133d97
igorhajduk35 <igorhajduk35@gmail.com>
2026-03-24 15:46:30 +0100

    Adding users to database

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,24 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import psycopg2
+import time
 
-db_connection = psycopg2.connect(
-    host="db",
-    database="mydb",
-    user="myuser",
-    password="mypassword"
-)
+while True:
+    try:
+        db_connection = psycopg2.connect(
+            host="db",
+            database="mydb",
+            user="myuser",
+            password="mypassword"
+        )
+
+        print("Connected to database")
+        break
+
+    except psycopg2.OperationalError:
+        print("Waiting for database connection..")
+        time.sleep(2)
+
 
 app = Flask(__name__)
 CORS(app)
@@ -19,6 +30,43 @@ def hello():
     cursor.execute("SELECT 'Hello from PostgreSQL!' ")
     result = cursor.fetchone()
     return jsonify({"message": result[0]})
+
+
+@app.route("/get_users")
+def get_users():
+    cursor = db_connection.cursor()
+    cursor.execute("SELECT id, username FROM users")
+    
+    results = cursor.fetchall()
+
+    if len(results) == 0:
+        return jsonify({"users": []})
+    
+
+    output = []
+
+    for result in results:
+        output.append({
+            "id": result[0],
+            "username": result[1]
+        })
+
+    return jsonify({"users": output})
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+    username = data.get("username")
+
+    cursor = db_connection.cursor()
+    cursor.execute(
+        "INSERT INTO users(username) VALUES (%s)",
+        (username,)
+    )
+
+    db_connection.commit()
+
+    return jsonify({"message": "User added!"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True, port=5050)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,52 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [message, setMessage] = useState("");
+  const [users, setUsers] = useState([]);
+  const [newUserName, setNewUserName] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:5050/api")
       .then(res => res.json())
       .then(data => setMessage(data.message))
+    console.log(typeof users, users);
   }, []);
 
+  const get_users = () => {
+    fetch("http://localhost:5050/get_users")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setUsers(data.users);
+      });
+  };
+
+  const add_user = () => {
+    fetch("http://localhost:5050/add_user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({username: newUserName})
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        get_users();
+      });
+  }
 
   return (
     <div className="App">
       <h1>React and Python</h1>
       <p>{message}</p>
+      <button onClick={get_users}>Load users</button>
+      <ul>
+        {Array.isArray(users) && users.map(user => (
+          <li key={user.id}>{user.username}</li>
+        ))}
+      </ul>
+      <input type="text" value={newUserName} onChange={e => setNewUserName(e.target.value)} placeholder="name of user"></input>
+      <button onClick={add_user}>Add user</button>
     </div>
   );
 }
